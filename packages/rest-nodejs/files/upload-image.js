1ef4322058e5245183302a41333e95be19073a9d
LKZ <lucas_vieira97@hotmail.com>
2021-12-13 16:11:11 -0300

    update upload image

--- packages/rest-nodejs/files/upload-image.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-
-fs.createReadStream('./assets/purple-sky.jpg')
-	.pipe(fs.createWriteStream('./assets/purple-sky-stream.jpg'))
-	.on('finish', () => console.log('Imagem foi escrita com sucesso'))
---
+++ packages/rest-nodejs/models/pets.js
@@ -0,0 +1,27 @@
+const conexao = require('../database/conection')
+const uploadFile = require('../files/uploadFile')
+
+class Pet {
+	adiciona(pet, res) {
+		const sql = 'INSERT INTO Pets SET ?'
+
+		uploadFile(pet.imagem, pet.nome, (erro, novoCaminho) => {
+			if (erro) {
+				res.status(400).json({ erro })
+			} else {
+				const novoPet = { nome: pet.nome, imagem: novoCaminho }
+
+				conexao.query(sql, novoPet, (erro) => {
+					if (erro) {
+						console.log(erro)
+						res.status(400).json(erro)
+					} else {
+						res.status(200).json(novoPet)
+					}
+				})
+			}
+		})
+	}
+}
+
+module.exports = new Pet()
