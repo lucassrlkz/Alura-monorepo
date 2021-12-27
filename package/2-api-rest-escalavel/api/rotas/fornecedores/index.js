dbd09d283e48f8906adff3bed2b7e5f218397d66
LKZ <lucas_vieira97@hotmail.com>
2021-12-27 15:30:55 -0300

    melhorando a api com status code

--- package/2-api-rest-escalavel/api/rotas/fornecedores/index.js
+++ package/2-api-rest-escalavel/api/rotas/fornecedores/index.js
@@ -1,9 +1,74 @@
 const router = require('express').Router()
-const TabelaFornecedor = require('./tabelaFornecedor')
+const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-router.use('/', async (req, res) => {
+router.get('/', async (req, res) => {
 	const resultados = await TabelaFornecedor.listar()
-	res.send(JSON.stringify(resultados))
+	res.status(200).send(JSON.stringify(resultados))
+})
+
+router.post('/', async (req, res) => {
+	try {
+		const dadosRecebidos = req.body
+		const fornecedor = new Fornecedor(dadosRecebidos)
+		await fornecedor.criar()
+		res.status(201).send(JSON.stringify(fornecedor))
+	} catch (erro) {
+		res.status(400).send(
+			JSON.stringify({
+				mensagem: erro.message,
+			})
+		)
+	}
+})
+
+router.get('/:idFornecedor', async (req, res) => {
+	try {
+		const id = req.params.idFornecedor
+		const fornecedor = new Fornecedor({ id: id })
+		await fornecedor.carregar()
+		res.status(200).send(JSON.stringify(fornecedor))
+	} catch (error) {
+		res.status(404).send(
+			JSON.stringify({
+				mensagem: error.message,
+			})
+		)
+	}
+})
+
+router.put('/:idFornceedor', async (req, res) => {
+	try {
+		const id = req.params.idFornceedor
+		const recebidos = req.body
+		const dados = Object.assign({}, recebidos, { id: id })
+		const fornecedor = new Fornecedor(dados)
+		await fornecedor.atualizar()
+		res.end()
+	} catch (error) {
+		res.status(400).send(
+			JSON.stringify({
+				mensagem: error.message,
+			})
+		)
+	}
+	res.end()
+})
+
+router.delete('/:idFornecedor', async (req, res) => {
+	try {
+		const id = req.params.idFornecedor
+		const fornecedor = new Fornecedor({ id: id })
+		await fornecedor.carregar()
+		await fornecedor.remover()
+		res.status(204).end()
+	} catch (erro) {
+		res.status(404).send(
+			JSON.stringify({
+				mensagem: error.message,
+			})
+		)
+	}
 })
 
 module.exports = router
--- package/2-api-rest-escalavel/api/rotas/fornecedores/tabelaFornecedor.js
+++ package/2-api-rest-escalavel/api/rotas/fornecedores/tabelaFornecedor.js
@@ -1,7 +1,34 @@
-const Modelo = require('./modeloTabelaFornecedor')
+const Modelo = require('./ModeloTabelaFornecedor')
 
 module.exports = {
 	listar() {
 		return Modelo.findAll()
+	},
+
+	inserir(fornecedor) {
+		return Modelo.create(fornecedor)
+	},
+
+	async pegarPorId(id) {
+		const encontrado = await Modelo.findOne({
+			where: { id: id },
+		})
+		if (!encontrado) {
+			throw new Error('Fornecedor não encontrado')
+		}
+
+		return encontrado
+	},
+
+	async atualizar(id, atualizar) {
+		return Modelo.update(atualizar, {
+			where: { id: id }
+		})
+	},
+
+	remover(id) {
+		return Modelo.destroy({
+			where: { id: id },
+		})
 	},
 }
