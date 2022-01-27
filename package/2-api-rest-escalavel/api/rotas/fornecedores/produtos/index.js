d672e2aa97031090ee25336d6672d7a5d7bb4805
lucassrlkz <lucas_vieira97@hotmail.com>
2022-01-27 20:00:31 -0300

    criacao de Produtos  e validacao

--- package/2-api-rest-escalavel/api/rotas/fornecedores/produtos/index.js
+++ package/2-api-rest-escalavel/api/rotas/fornecedores/produtos/index.js
@@ -1,9 +1,36 @@
-const router = require('express').Router({mergeParams: true})
+const router = require('express').Router({ mergeParams: true })
 const Tabela = require('./tabelaProduto')
+const Produto = require('./Produto')
 
-router.get('/', async (req, res)=>{
-    const produtos = await Tabela.listar(req.params.idFornecedor)
-    res.send(produtos)
+router.get('/', async (req, res) => {
+	const produtos = await Tabela.listar(req.params.idFornecedor)
+	res.send(produtos)
 })
 
+router.post('/', async (req, res, next) => {
+	try {
+        const idFornecedor = req.params.idFornecedor
+        const body = req.body
+
+        const dados = { ...body, fornecedor: idFornecedor }
+        const produto = new Produto(dados)
+
+        await produto.criar(dados)
+
+        res.status(201).send(produto)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete('/:idProduto', async (req, res) => {
+	const dados = {
+        fornecedor: req.params.idFornecedor,
+        id: req.params.idProduto
+    }
+    const produto = new Produto(dados)
+    await produto.deletar()
+
+    res.status(204).end()
+})
 module.exports = router
