34906e49485ef60a8ffcd3645956ac50bea6272c
lucassrlkz <lucas_vieira97@hotmail.com>
2022-01-27 17:43:51 -0300

    updating

--- package/2-api-rest-escalavel/api/database/criarTabelas.js
+++ package/2-api-rest-escalavel/api/database/criarTabelas.js
@@ -1,13 +1,17 @@
 const modelos = [
 	require('../rotas/fornecedores/modeloTabelaFornecedor'),
-	require('../rotas/fornecedores/produtos/modeloTabelaProduto')
+	require('../rotas/fornecedores/produtos/modeloTabelaProduto'),
 ]
 
-async function criarTabelas(){
-	for(let contador =0; contador< modelos.length; contador++){
-		const modelo = modelos[contador]
+async function criarTabelas() {
+	modelos.forEach((modelo) => {
 		await modelo.sync()
-	}
+	})
 }
-
+// async function criarTabelas() {
+// 	for (let contador = 0; contador < modelos.length; contador++) {
+// 		const modelo = modelos[contador]
+// 		await modelo.sync()
+// 	}
+// }
 criarTabelas()
