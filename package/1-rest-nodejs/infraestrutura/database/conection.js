56ce39221245b4b87a4f0a4e7883acb0459bc5b0
lucassrlkz <lucas_vieira97@hotmail.com>
2022-01-29 21:41:07 -0300

    refatoracao usando config

--- package/1-rest-nodejs/infraestrutura/database/conection.js
+++ package/1-rest-nodejs/infraestrutura/database/conection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
+const config = require('config')
 
 const conexao = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'root',
-	database: 'agenda-petshop',
+	host: config.get('mysql.host'),
+	port: config.get('mysql.port'),
+	user: config.get('mysql.username'),
+	password: config.get('mysql.password'),
+	database: config.get('mysql.database'),
 })
 
 module.exports = conexao
