1a9e5b2c1d5842f58da0a94c0e901a7037cd4b1b
lucassrlkz <lucas_vieira97@hotmail.com>
2022-03-15 16:29:03 -0300

    finishing part 1

--- package/4-typescript-p1/app/models/negociacao.ts
+++ package/4-typescript-p1/app/models/negociacao.ts
@@ -1,23 +1,16 @@
 export class Negociacao {
 	constructor(
 		private _data: Date,
-		private _quantidade: number,
-		private _valor: number) {
+		public readonly quantidade: number,
+		public readonly valor: number) {
 	}
 
 	get data(): Date {
-		return this._data
-	}
-
-	get quantidade(): Number {
-		return this._quantidade
-	}
-
-	get valor(): Number {
-		return this._valor
+		const data = new Date(this._data.getTime())
+		return data
 	}
 
 	get volume(): Number {
-		return this._quantidade * this._valor
+		return this.quantidade * this.valor
 	}
 }
